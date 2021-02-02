cbf64840f9326fca4172a8cfe95b43c99b5b1651
greedy17 <edwardroberson17@gmail.com>
2021-02-02 18:42:02 -0500

    added search bar to Navbar component

--- src/Components/NavBar/navBar.jsx
+++ src/Components/NavBar/navBar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Navbar,Nav,Button} from 'react-bootstrap';
+import {Navbar,Nav,Button,Form,FormControl} from 'react-bootstrap';
 import greenlogo from '../../assets/images/greenlogo.png'
 import './navBar.css'
 
@@ -18,13 +18,17 @@ const NavBar = () => {
             </Navbar.Brand>
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
-                <Nav className="mr-auto">
+                <Nav>
                 </Nav>
                 <Nav>
                 <Nav.Link href="sign"><Button className="white" variant="muted">Sign up</Button></Nav.Link>    
-                <Nav.Link href="log"><Button className="green">Log in</Button></Nav.Link>
+                <Nav.Link href="log"><Button className="white" variant="muted">Log in</Button></Nav.Link>
                 </Nav>
             </Navbar.Collapse>
+            <Form inline>
+            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+            <Button className="green">Search</Button>
+            </Form>
             </Navbar>
         </div>
     )
