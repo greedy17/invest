c5baa2ffa0a91de95de4c1c94dd1a8cd38df03cb
greedy17 <edwardroberson17@gmail.com>
2021-02-02 21:04:55 -0500

    created post endpoint

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 const connectDB = require('./startup/db');
 const express = require('express');
+const products = require('./routes/product');
 const app = express();
 
 connectDB();
 
 app.use(express.json());
+app.use('/api/products', products);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
--- server/routes/product.js
+++ server/routes/product.js
@@ -2,4 +2,20 @@ const Product = require('../models/product');
 const express = require('express');
 const router = express.Router();
 
+router.post('/', async (req,res) => {
+    try{
+        const product = new Product({
+            name: 'Cool product Number 1',
+            category: "Cool",
+            goal: 5,
+        });
+
+        await product.save();
+
+        return res.send(product);
+    } catch(ex){
+        return res.status(500).send(`Internal Server Error: ${ex}`);
+    }
+});
+
 module.exports = router;
