ab180ca2eea7c6cbce6d24fea5309e53199d03a4
greedy17 <edwardroberson17@gmail.com>
2021-02-04 21:19:17 -0500

    created POST route handler for the User and added hashing for the user password.

--- server/models/user.js
+++ server/models/user.js
@@ -1,6 +1,9 @@
 const mongoose = require("mongoose");
 const Joi = require('joi');
 const { productSchema } = require('./product');
+const bcrypt = require('bcrypt');
+const express = require('express');
+const router = express.Router();
 
 const userSchema = new mongoose.Schema({
     name: { type: String, required: true, minlength:1, maxlength: 50},
@@ -18,6 +21,29 @@ function validateUser(user) {
     });
     return schema.validate(user);
 }
+
+router.post('/', async(req, res) => {
+    try{
+        const {error} = validateUser(req.body);
+
+        if(error) return res.status(400).send(error.details[0].message);
+
+        let user = await User.findOne({email: req.body.email});
+        if(user) return res.status(400).send('User already registered.');
+
+        const salt = await bcrypt.genSalt(10);
+        user = new User({
+            name: req.body.name,
+            email: req.body.email,
+            password: await bcrypt.hash(req.body.password, salt),
+        });
+
+        await user.save();
+        return res.send({_id: user._id, name: user.name, email: user.email});
+    } catch(ex) {
+        return res.status(500).send(`Internal Server Error: ${ex}`);
+    }
+});
 
 exports.User = User;
 exports.validateUser = validateUser;
